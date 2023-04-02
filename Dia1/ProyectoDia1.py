5526f86fcd987b0432ccf8ecc053ce04d324ae37
Javier <javierpampliega2002@gmail.com>
2023-04-02 22:32:41 +0200

    Dia1/ProyectoDia1: Crear nombre de una cerveza

---
+++ Dia1/ProyectoDia1.py
@@ -0,0 +1,8 @@
+#Proyecto del Dia1
+#Autor: Javier Pampliega Garcia
+#Version:1.0
+
+#Problema: Se debe de crear un programa que haga dos preguntas al usuario, para crear el nombre de una cerveza.
+
+#El print se realiza de dentro hacia fuera, es decir, se hacen primero los input y luego se imprime todo.
+print("El nombre de la cerveza es: " + input("Introduce una palabra en ingles: ") + " " + input("Introduce el gentilicio de un pais: "))
