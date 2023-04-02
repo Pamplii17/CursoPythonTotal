512e6f18e94d2d7673c8bc6728d18433212d4478
Javier <javierpampliega2002@gmail.com>
2023-04-02 22:27:45 +0200

    Dia1/INPUT

---
+++ Dia1/input.py
@@ -0,0 +1,19 @@
+#En este archivo se explica la funcion input
+
+print("Dime tu nombre:")
+print("Dime tu apellido:")
+
+#Solicita el nombre y el apellido y permite al usuario introducirlos
+input("Dime tu nombre:")
+input("Dime tu apellido:")
+
+#Imprime tambien lo que se le pasa al input
+print(input("Dime tu nombre:"))
+print(input("Dime tu apellido:"))
+
+
+print("Tu nombre es:" + input("Dime tu nombre:"))
+print("Tu apellido es:" + input("Dime tu apellido:"))
+
+#Imprime todo lo deseado
+print("Tu nombre es: " + input("Dime tu nombre: ") + " " + input("Dime tu apellido: "))
