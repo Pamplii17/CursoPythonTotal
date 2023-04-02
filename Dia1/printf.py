ed1f0a0bec29620ef9f63e52357acbf19d905286
Javier <javierpampliega2002@gmail.com>
2023-04-02 22:00:30 +0200

    Dia1/PRINT

--- Dia1/printf.py
+++ Dia1/printf.py
@@ -1,2 +1,15 @@
 #En este archivo se va a aprender la funcion print
 
+print("Hola mundo")
+
+#Print puede imprimir comillas simples, si queremos entrecomillar una cadena de caracteres podemos usar comillas simples
+print("Hola mundo te digo 'Hola'")
+
+#Puede imprimir numeros
+print(100)
+
+#Puede imprimir el resultado de operaciones
+print(100+50)
+
+#Imprime la operacion como tal, NO SU RESULTADO
+print("100+50")
