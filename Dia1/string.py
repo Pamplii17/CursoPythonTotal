68f95df736438e61ae9d60096b8f52b660df54c0
Javier <javierpampliega2002@gmail.com>
2023-04-02 22:10:11 +0200

    Dia1/STRINGS

---
+++ Dia1/string.py
@@ -0,0 +1,19 @@
+#En este archivo se estudiaran los strings
+
+print("Javier")
+
+#El simbolo "+" concatena cadenas de caracteres
+print("Hola " + "Javier")
+print("Hola" + " " + "Javier")
+
+#Con "\"" se pueden poner las comillas sin que salga error
+print("Me llamo \"Javier\"")
+
+#Con "\n" se puede imprimir un salto de linea
+print("Esta es una linea\nY esta es otra linea")
+
+#Con "\t" se puede tabular
+print("\tEsta linea esta tabulada")
+
+#Con "\\" puedo imprimir una barra invertida
+print("Este signo \\ es una barra invertida")
